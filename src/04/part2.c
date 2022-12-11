71e5b40518125dda723aa3bbcd8dfbffcca3af2a
Martin Stoev <martin.y.stoev@gmail.com>
2022-12-11 15:44:27 +0200

    day 4

---
+++ src/04/part2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+  FILE * fp;
+  char * line = NULL;
+  size_t len = 0;
+  ssize_t read;
+
+  fp = fopen("input.txt", "r");
+  if (fp == NULL) {
+    return 0;
+  }
+
+  int numberOverlaps = 0;
+  while ((read = getline(&line, &len, fp)) != -1) {
+    char *firstPair = strtok(line, ",");
+    char *secondPair = strtok(NULL, ",");
+
+    int a1 = atoi(strtok(firstPair, "-"));
+    int a2 = atoi(strtok(NULL, "-"));
+
+    int b1 = atoi(strtok(secondPair, "-"));
+    int b2 = atoi(strtok(NULL, "-"));
+
+    if ((a1 >= b1 && a1 <= b2) || (a2 >= b1 && a2 <= b2)) {
+      numberOverlaps++;
+    } else if ((b1 >= a1 && b1 <= a2) || (b2 >= a1 && b2 <= a2)) {
+      numberOverlaps++;
+    }
+  }
+
+  printf("Number overlaps: %d\n", numberOverlaps);
+
+  fclose(fp);
+  return 0;
+}
